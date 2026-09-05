9be6c57a091c3857ededafc47ba49cb2b7968226
aeilkema <6303158+aeilkema@users.noreply.github.com>
2026-09-05 16:11:24 +0200

    eMule Next: add settings view interface

---
+++ srchybrid/EmuleNextSettingsWnd.h
@@ -0,0 +1,38 @@
+//this file is part of eMule Next
+//Copyright (C)2026 eMule Next contributors
+//GPL v2 or later
+#pragma once
+
+static const uint32 EMULENEXT_SETTINGS_VIEW_ID = 0x7FFFFF04u;
+
+class CEmuleNextSettingsWnd : public CWnd
+{
+public:
+    CEmuleNextSettingsWnd();
+    virtual ~CEmuleNextSettingsWnd();
+
+    bool Create(CWnd* parent);
+    void Refresh();
+
+protected:
+    DECLARE_MESSAGE_MAP()
+    afx_msg int OnCreate(LPCREATESTRUCT createStruct);
+    afx_msg void OnSize(UINT type, int cx, int cy);
+    afx_msg BOOL OnEraseBkgnd(CDC* dc);
+    afx_msg HBRUSH OnCtlColor(CDC* dc, CWnd* wnd, UINT ctlColor);
+    afx_msg void OnApplyClicked();
+
+private:
+    void LayoutControls(int cx, int cy);
+
+    CStatic m_heading;
+    CStatic m_themeLabel;
+    CComboBox m_themeMode;
+    CStatic m_discoveryLabel;
+    CButton m_discoveryEnabled;
+    CStatic m_concurrencyLabel;
+    CComboBox m_maxConcurrent;
+    CButton m_apply;
+    CStatic m_status;
+    CBrush m_darkBrush;
+};
